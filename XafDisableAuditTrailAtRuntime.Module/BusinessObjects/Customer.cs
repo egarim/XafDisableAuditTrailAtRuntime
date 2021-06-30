74af2f58160af709fdf078f50ff2a551c803b101
Jose Ojeda <joche.ojeda@bitframeworks.com>
2021-06-30 10:45:44 +0300

    Add project files.

---
+++ XafDisableAuditTrailAtRuntime.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,54 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafDisableAuditTrailAtRuntime.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Customer : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public Customer(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        string phone;
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+        
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Phone
+        {
+            get => phone;
+            set => SetPropertyValue(nameof(Phone), ref phone, value);
+        }
+    }
+}
---
+++ XafDisableAuditTrailAtRuntime.Module/Controllers/DisableAuditController.cs
@@ -0,0 +1,75 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.AuditTrail;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XafDisableAuditTrailAtRuntime.Module.BusinessObjects;
+
+namespace XafDisableAuditTrailAtRuntime.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class DisableAuditController : ViewController
+    {
+        SimpleAction EnableAudit;
+        SimpleAction DisableAudit;
+        public DisableAuditController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            DisableAudit = new SimpleAction(this, "Disable audit", "View");
+            DisableAudit.Execute += DisableAudit_Execute;
+
+            EnableAudit = new SimpleAction(this, "Enable audit", "View");
+            EnableAudit.Execute += EnableAudit_Execute;
+            
+
+        }
+        private void EnableAudit_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            var Customer = this.ObjectSpace.CreateObject<Customer>();
+            Customer.Name = "Javier";
+            this.ObjectSpace.CommitChanges();
+        }
+        private void DisableAudit_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            //https://supportcenter.devexpress.com/ticket/details/q568049/audittrail-how-to-switch-it-off-for-a-while-and-on-again-at-runtime#
+            // Execute your business logic.
+
+            AuditTrailService.Instance.SaveAuditTrailData += Instance_SaveAuditTrailData;
+            var Customer= this.ObjectSpace.CreateObject<Customer>();
+            Customer.Name = "Joche";
+            this.ObjectSpace.CommitChanges();
+            AuditTrailService.Instance.SaveAuditTrailData -= Instance_SaveAuditTrailData;
+        }
+        void Instance_SaveAuditTrailData(object sender, SaveAuditTrailDataEventArgs e)
+        {
+            e.Handled = true;
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
